0a9601deddc31a842f75a4bcaabade6a5b211069
Philippe GALVAN <git.philippeG@outlook.com>
2022-05-09 22:40:56 +0200

    refactor: modify flow declaration from low to higher level abstraction

--- src/flows.py
+++ src/flows.py
@@ -37,23 +37,20 @@ combine_enim_accidents_with_ship_flow.set_dependencies(
 
 enhance_seamis_reports_flow = Flow("enhance_seamis_reports_flow")
 
-enhance_seamis_reports_flow.add_edge(
-    tasks.extract_seamis_reports,
-    tasks.extract_ship_data_from_seamis_reports,
-    "seamis_reports",
+enhance_seamis_reports_flow.set_dependencies(
+    task=tasks.extract_ship_data_from_seamis_reports,
+    keyword_tasks=dict(seamis_reports=tasks.extract_seamis_reports),
 )
-enhance_seamis_reports_flow.add_edge(
-    tasks.extract_ship_data_from_seamis_reports,
-    tasks.combine_seamis_reports_with_ships,
-    "available_ships",
+enhance_seamis_reports_flow.set_dependencies(
+    task=tasks.combine_seamis_reports_with_ships,
+    keyword_tasks=dict(
+        seamis_reports=tasks.extract_seamis_reports,
+        available_ships=tasks.extract_ship_data_from_seamis_reports,
+    ),
 )
-enhance_seamis_reports_flow.add_edge(
-    tasks.extract_seamis_reports,
-    tasks.combine_seamis_reports_with_ships,
-    "seamis_reports",
-)
-enhance_seamis_reports_flow.add_edge(
-    tasks.combine_seamis_reports_with_ships,
-    tasks.load_seamis_reports_with_ship,
-    "enhanced_seamis_reports",
+enhance_seamis_reports_flow.set_dependencies(
+    task=tasks.load_seamis_reports_with_ship,
+    keyword_tasks=dict(
+        enhanced_seamis_reports=tasks.combine_seamis_reports_with_ships
+    ),
 )
