9216d3fa354f741676751eac9e8096bbdb70134a
Philippe GALVAN <git.philippeG@outlook.com>
2022-05-09 22:36:09 +0200

    black

--- src/flows.py
+++ src/flows.py
@@ -6,7 +6,10 @@ from config import Config
 combine_enim_accidents_with_ship_flow = Flow("combine_enim_accidents_with_ship_flow")
 
 
-enim_accident_client_param = Parameter(name="enim_accident_client", default=Config.ENIM_ACCIDENT_CLIENT,)
+enim_accident_client_param = Parameter(
+    name="enim_accident_client",
+    default=Config.ENIM_ACCIDENT_CLIENT,
+)
 combine_enim_accidents_with_ship_flow.set_dependencies(
     task=tasks.extract_enim_accidents,
     keyword_tasks=dict(enim_accident_client_implementation=enim_accident_client_param),
--- src/tasks/task_extract_enim_accidents.py
+++ src/tasks/task_extract_enim_accidents.py
@@ -6,7 +6,9 @@ from src.repositories import enim_accidents_client_factory
 
 class ExtractEnimAccidents(Task):
     def run(self, enim_accident_client_implementation: str) -> list[AccidentEnim]:  # type: ignore
-        enim_accident_client = enim_accidents_client_factory(enim_accident_client_implementation)
+        enim_accident_client = enim_accidents_client_factory(
+            enim_accident_client_implementation
+        )
         return enim_accident_client.get_all()
 
 
